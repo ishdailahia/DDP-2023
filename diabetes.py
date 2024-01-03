a01be7b38c3bdb6ff0029ac354b40d17feec98ea
ishdailahia <146152477+ishdailahia@users.noreply.github.com>
2024-01-03 10:43:03 +0700

    project-DDP-2023

---
+++ diabetes.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+
+root = Tk()
+root.configure(bg="aqua")
+root.title("Aplikasi Diabetes")
+
+def hitung_risiko():
+    gula_darah = float(entry1.get())
+    tekanan_darah = float(entry2.get())
+    
+    if gula_darah < 70:
+        hasil = "Level gula darah rendah. Segera konsultasikan dengan dokter."
+    elif 70 <= gula_darah <= 130:
+        hasil ="Level gula darah normal Dan Tidak Mengalami Diabetes"
+    elif 130 <= gula_darah <= 200:
+        hasil = "Level gula darah tinggi. Segera konsultasikan dengan dokter Dan Akan Mengalami Resiko Diabetes"
+    else:
+        hasil = "Anda Terkena Diabetes."
+
+    hasil1["text"] = hasil
+
+gula_darah = Label(root, text="Kadar Gula Darah:")
+gula_darah.grid(column=0, row=0, padx=10, pady=10)
+entry1 = Entry(root)
+entry1.grid(column=1, row=0, padx=10, pady=10)
+
+tekanan_darah = Label(root, text="Tekanan Darah:")
+tekanan_darah.grid(column=0, row=1, padx=10, pady=10)
+entry2 = Entry(root)
+entry2.grid(column=1, row=1, padx=10, pady=10)
+
+btn = Button(root, text="Hitung Risiko", command=hitung_risiko)
+btn.grid(column=0, row=2, columnspan=2, pady=10)
+
+hasil1 = Label(root, text="")
+hasil1.grid(column=0, row=3, columnspan=2, pady=10)
+
+root.mainloop()
